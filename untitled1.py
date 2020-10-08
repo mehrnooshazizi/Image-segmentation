fa072e0ff07c2f4427c0bd6428ad0f9c9c8fc7ba
mehrnooshazizi <61627074+mehrnooshazizi@users.noreply.github.com>
2020-10-08 18:23:24 +0330

    update

--- untitled1.py
+++ untitled1.py
@@ -9,6 +9,6 @@ Created on Wed Oct  7 15:33:53 2020
 import cv2
 import numpy as np
 
-rgb = np.uint8([[[17,1,126]]])
+rgb = np.uint8([[[255,255,0]]])
 hsv = cv2.cvtColor(rgb, cv2.COLOR_BGR2HSV)
 print(hsv)
